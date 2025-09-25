2399fac07b12d53797057416bc7de30db831859c
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-09-25 23:42:47 +0700

    tic tac -toe

---
+++ tic-tac-toe/src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import  './TicTacToe.css';
+import cross_icon from '../Assets/cross.png';
+import cicle_icon from '../Assets/circle.png';
+
+let data = ["", "", "", "", "", "", "", "", ""];
+
+const TicTacToe = () => {
+
+    const [count, setCount] = useState(0)
+    const [lock, setLock] = useState(false)
+
+    const toggle = (e, num) => {
+        if(lock) {
+            return 0;
+        }
+        if(count % 2 === 0) {
+            e.target.innerHTML = `<img src='${cross_icon}' />`
+            data[num] = "X";
+            setCount(++count);
+        } else {
+            e.target.innerHTML = `<img src='${cicle_icon}' />`
+            data[num] = "O";
+            setCount(++count);
+        }
+    }
+
+    return (
+        <div className='container'>
+            <h1 className='title'>Tic Tac Toe in <span>React</span></h1>
+            <div className='board'>
+                <div className='row1'>
+                    <div className='boxes' onClick={(e) => toggle(e, 0)}></div>
+                    <div className='boxes' onClick={(e) => toggle(e, 1)}></div>
+                    <div className='boxes' onClick={(e) => toggle(e, 2)}></div>
+                </div>
+                <div className='row2'>
+                    <div className='boxes' onClick={(e) => toggle(e, 3)}></div>
+                    <div className='boxes' onClick={(e) => toggle(e, 4)}></div>
+                    <div className='boxes' onClick={(e) => toggle(e, 5)}></div>
+                </div>
+                <div className='row3'>
+                    <div className='boxes' onClick={(e) => toggle(e, 6)}></div>
+                    <div className='boxes' onClick={(e) => toggle(e, 7)}></div>
+                    <div className='boxes' onClick={(e) => toggle(e, 8)}></div>
+                </div>
+            </div>
+            <button className='reset'>Reset</button>
+        </div>
+    );
+};
+
+export default TicTacToe;
